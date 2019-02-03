0b0412dcb15d6db2a6470de3af80031d06a2b77b
Fran31416 <gps112@gmail.com>
2019-02-03 10:32:48 +0100

    Cambiado

--- lista.js
+++ lista.js
@@ -30,3 +30,66 @@ window.addEventListener("load",()=>{
 	}
 });
 
+//Muestra la página "lista" en función de la lista a ver, el usuario que la ve y los permisos que tiene
+function mostrarLista(usuario,permiso,lista) {
+	generarBotones(usuario,permiso,lista);
+	generarLista(usuario,permiso,lista);
+}
+
+function generarBotones(usuario,permiso,lista){
+
+}
+
+
+//Muestra la lista de documentos que cierto usuario con cierto permiso puede ver
+function generarLista(usuario,permiso,lista){
+	switch (permiso) {
+		case "Profesor":
+			pideDatos("peticion","?estado_proceso="+lista+"&usuario="+usuario,(data) => {
+					//Convertimos a JSON los datos obtenidos
+					let json = JSON.parse(data);
+					//Colocamos cada permiso en su lugar
+					console.log(json);
+					printLista({"lista":json},"#lista");
+					//json=JSON.parse(data);
+				}
+			);
+			break;
+		case "Directivo":
+			if (lista==="1" || lista==="3"){
+				pideDatos("peticion","?estado_proceso="+lista+"&usuario="+usuario,(data) => {
+						//Convertimos a JSON los datos obtenidos
+						let json = JSON.parse(data);
+						//Colocamos cada permiso en su lugar
+						console.log(json);
+						printLista({"lista":json});
+						//json=JSON.parse(data);
+					}
+				);
+			} else {
+				pideDatos("peticion","?estado_proceso="+lista,(data) => {
+						//Convertimos a JSON los datos obtenidos
+						let json = JSON.parse(data);
+						//Colocamos cada permiso en su lugar
+						console.log(json);
+						printLista({"lista":json});
+						//json=JSON.parse(data);
+					}
+				);
+			}
+			break;
+		case "Admin":
+			pideDatos("peticion","?estado_proceso="+lista,(data) => {
+					//Convertimos a JSON los datos obtenidos
+					let json = JSON.parse(data);
+					//Colocamos cada permiso en su lugar
+					console.log(json);
+					printLista({"lista":json});
+					//json=JSON.parse(data);
+				}
+			);
+			break;
+	}
+}
+
+
